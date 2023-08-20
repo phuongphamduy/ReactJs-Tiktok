9d2c4ac7016f01e3e46ccfe53d275c1ed88f5d84
phuongphamduy <92977825+phuongphamduy@users.noreply.github.com>
2023-08-20 22:38:29 +0700

    Cấu hình Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { publicRoutes } from '~/route';
+import { DefaultLayout, HeaderOnly } from '~/components/Layout';
+import { Fragment } from 'react';
+
 function App() {
     return (
-        <div className="App">
-            <h1>Reat App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                {
+                    <Routes>
+                        {publicRoutes.map((item, index) => {
+                            const Page = item.component;
+                            let Layout = DefaultLayout;
+
+                            if (item.layout) {
+                                Layout = item.layout;
+                            } else if (item.layout === null) {
+                                Layout = Fragment;
+                            }
+
+                            return (
+                                <Route
+                                    path={item.path}
+                                    element={
+                                        <Layout>
+                                            <Page />
+                                        </Layout>
+                                    }
+                                />
+                            );
+                        })}
+                    </Routes>
+                }
+            </div>
+        </Router>
     );
 }
 
